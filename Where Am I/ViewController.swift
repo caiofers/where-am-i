935e6d11e8b7af9addaf68be6cd767512bda8610
Caio <caiovr13@gmail.com>
2021-03-25 22:48:13 -0300

    Endereço e ajustes no layout

--- Where Am I/ViewController.swift
+++ Where Am I/ViewController.swift
@@ -12,8 +12,10 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     @IBOutlet weak var latitudeValue: UILabel!
     @IBOutlet weak var longitudeValue: UILabel!
     @IBOutlet weak var adressValue: UILabel!
+    @IBOutlet weak var speedView: UIStackView!
     @IBOutlet weak var speedValue: UILabel!
     @IBOutlet weak var mapView: MKMapView!
+    
     
     private var locationManager = CLLocationManager()
     
@@ -57,18 +59,88 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         
         longitudeValue.text = String(longitude!)
         latitudeValue.text = String(latitude!)
-        speedValue.text = String(userLocalization!.speed)
+        
+        if userLocalization!.speed > 0 {
+            speedView.isHidden = false
+            speedValue.text = String(userLocalization!.speed)
+        } else {
+            speedView.isHidden = true
+        }
         
         let deltaLatitude: CLLocationDegrees = 0.01
         let deltaLongitude: CLLocationDegrees = 0.01
         
-        let location = CLLocationCoordinate2D(latitude: latitude!, longitude: longitude!)
+        let location2D = CLLocationCoordinate2D(latitude: latitude!, longitude: longitude!)
+        let location = CLLocation(latitude: latitude!, longitude: longitude!)
         
         let visualizationArea: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: deltaLatitude, longitudeDelta: deltaLongitude)
         
-        let region: MKCoordinateRegion = MKCoordinateRegion(center: location, span: visualizationArea)
+        let region: MKCoordinateRegion = MKCoordinateRegion(center: location2D, span: visualizationArea)
         
         mapView.setRegion(region, animated: true)
+        
+        CLGeocoder().reverseGeocodeLocation(location, preferredLocale: nil) { (localDetails, error) in
+            if error == nil {
+                if let local = localDetails?.first {
+                    // Rua
+                    var thoroughfare = ""
+                    if local.thoroughfare != nil {
+                        thoroughfare = local.thoroughfare!
+                    }
+                    
+                    // Número
+                    var subThoroughfare = ""
+                    if local.subThoroughfare != nil {
+                        subThoroughfare = local.subThoroughfare!
+                    }
+                    
+                    // Estado
+                    var locality = ""
+                    if local.locality != nil {
+                        locality = local.locality!
+                    }
+                    
+                    // Cidade
+                    var subLocality = ""
+                    if local.subLocality != nil {
+                        subLocality = local.subLocality!
+                    }
+                    
+                    // CEP
+                    var postalCode = ""
+                    if local.postalCode != nil {
+                        postalCode = local.postalCode!
+                    }
+                    
+                    // País
+                    var country = ""
+                    if local.country != nil {
+                        country = local.country!
+                    }
+                    
+                    // Sigla Estado
+                    var administrativeArea = ""
+                    if local.postalCode != nil {
+                        administrativeArea = local.administrativeArea!
+                    }
+                    
+                    var subAdministrativeArea = ""
+                    if local.subAdministrativeArea != nil {
+                        subAdministrativeArea = local.subAdministrativeArea!
+                    }
+                    
+                    self.adressValue.text = (thoroughfare == "" ? "" : thoroughfare + ", ") +
+                                            (subThoroughfare == "" ? "" : subThoroughfare + ", ") +
+                                            (subLocality == "" ? "" : subLocality + "-") +
+                                            (administrativeArea == "" ? "" : administrativeArea + ", ") +
+                                            (country == "" ? "" : country) +
+                                            (postalCode == "" ? "" : "\nZip Code: " + postalCode)
+                }
+            } else {
+                // TODO: Error
+                print ("ERROR")
+            }
+        }
         
     }
 
